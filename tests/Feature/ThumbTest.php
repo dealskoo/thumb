c825263035c1002cb4e867cddd2e92c64235ff3d
jabber <2898117012@qq.com>
2022-06-09 06:36:26 +0800

    init

---
+++ tests/Feature/ThumbTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dealskoo\Thumb\Tests\Feature;
+
+use Dealskoo\Thumb\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThumbTest extends TestCase
+{
+    use RefreshDatabase;
+}
